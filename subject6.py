b87c10fa087d9af737e4ef79feb27d5b5b56ff18
Yang Minshun <yangminshun@protonmail.com>
2020-06-24 22:06:52 +0900

    New

---
+++ subject6.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class MultiNewton(object):
+    def __init__(self, f, dx0_f, dx1_f, grad_f, hessian_matrix):
+        self.f = f
+        self.dx0_f = dx0_f
+        self.dx1_f = dx1_f
+        self.grad_f = grad_f
+        self.hessian_matrix = hessian_matrix
+
+    def compute_dx(self, bar_x):
+        grad = self.grad_f(bar_x)
+        hessian_matrix = self.hessian_matrix(bar_x)
+        dx = np.linalg.solve(hessian_matrix, - grad)
+        return dx
+
+    def solve(self, init_x, n_iter=100, tol=0.01):
+        self.hist = np.zeros(n_iter)
+        bar_x = init_x
+
+        for i in range(n_iter):
+            dx = self.compute_dx(bar_x)
+            x = bar_x + dx
+            print(f"x({i + 1}) = [{x[0]:.2f} {x[1]:.2f}]")
+            bar_x = x
+            norm_dx = np.linalg.norm(dx)
+            self.hist[i] += norm_dx
+
+            if norm_dx < tol:
+                self.hist = self.hist[:i]
+                break
+        return x
+
+
+def main():
+    f = lambda x: 10 * (x[1] ** 2) + 10 * (x[0] ** 4) - 20 * (x[0] ** 2) * x[1] + 1 + x[0] ** 2 - 2 * x[0]
+    dx0_f = lambda x: 40 * (x[0] ** 3) - 40 * x[0] * x[1] + 2 * x[0] - 2
+    dx1_f = lambda x: 20 * x[1] - 20 * (x[0] ** 2)
+    grad_f = lambda x: np.array([dx0_f(x), dx1_f(x)])
+    hessian_matrix = lambda x: np.array([[120 * (x[0] ** 2) - 40 * x[1] + 2, 40 * x[0]], [40 * x[0], 20 - 40 * x[0]]])
+
+    init_x = np.array([-2, 2])
+    solver = MultiNewton(f, dx0_f, dx1_f, grad_f, hessian_matrix)
+    res = solver.solve(init_x=init_x, n_iter=5)
+    print(f"Solution is x = [{res[0]:.2f} {res[1]:.2f}]")
+
+    errors = solver.hist
+    epochs = np.arange(0, errors.shape[0])
+
+    plt.rcParams["font.size"] = 13
+    plt.plot(epochs, errors)
+    plt.tight_layout()
+    plt.savefig('subject6.png')
+
+
+if __name__ == "__main__":
+    main()
